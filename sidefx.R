f5226c0c5c9e881bf64286ac5982c82406a927d1
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-05-08 08:23:41 +0200

    back 2 sep args and formals

--- sidefx.R
+++ sidefx.R
@@ -3,9 +3,17 @@
 hasSFX <- function(func, recursive=FALSE) {
   stopifnot(is.function(func), is.logical(recursive))
   # scan input to character vectors
-  sig <- paste0(deparse(args(func)), collapse='')
-  spl <- strsplit(sub('^.*\\(([^\\)]*)\\).*$', '\\1', sig, perl=TRUE), ',')
-  params <- gsub('^\\s*|\\s*$', '', spl[[1]], perl=TRUE)
+  params <- if (is.primitive(func)) {
+    sig <- paste0(deparse(args(func)), collapse='')
+    spl <- strsplit(sub('^.*\\(([^\\)]*)\\).*$', '\\1', sig, perl=TRUE), 
+                    ',', fixed=TRUE)
+    
+    gsub('^\\s*|\\s*$', '', spl[[1]], perl=TRUE)
+    # cast chr vector to list !!!
+    
+  } else {
+    formals(func)
+  }
   fbody <- if (is.primitive(func)) {
     sub('^.*function\\s\\([^\\)]*\\)\\s*', '', capture.output(func), perl=TRUE)
   } else {
@@ -14,5 +22,7 @@ hasSFX <- function(func, recursive=FALSE) {
   # get all sidefx yielding builtins in a vector
   stdlib <- builtins()
   # ...
+  # identify sidefx and inner functions in tokens !!!
+ #token_df <- sourcetools::tokenize_string(paste0('{', fbody, '}'))
   list(params, fbody)
 }
